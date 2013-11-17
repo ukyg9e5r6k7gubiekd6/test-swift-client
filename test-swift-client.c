625cf9134169663a36a5d5da8d3559d02ef169ea
Matt <ukyg9e5r6k7gubiekd6@yahoo.com>
2013-11-17 20:18:51 +1100

    Use new keystone-client library.

--- test-swift-client.c
+++ test-swift-client.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "keystone-client.h"
 #include "swift-client.h"
 
 #if 0
@@ -20,46 +21,36 @@
 #define USAGE "Usage: %s <tenant-name> <username> <password>"
 
 static enum swift_error
-test_swift_client(swift_context_t *context, const char *keystone_url, const char *tenant_name, const char *username, const char *password)
+test_swift_client(swift_context_t *swift)
 {
 	enum swift_error scerr;
 
-	scerr = swift_set_proxy(context, PROXY);
+	scerr = swift_set_container(swift, SWIFT_CONTAINER);
 	if (scerr != SCERR_SUCCESS) {
 		return scerr;
 	}
 
-	scerr = keystone_authenticate(context, keystone_url, tenant_name, username, password);
+	scerr = swift_create_container(swift);
 	if (scerr != SCERR_SUCCESS) {
 		return scerr;
 	}
 
-	scerr = swift_set_container(context, SWIFT_CONTAINER);
+	scerr = swift_set_object(swift, SWIFT_OBJECT);
 	if (scerr != SCERR_SUCCESS) {
 		return scerr;
 	}
 
-	scerr = swift_create_container(context);
+	scerr = swift_put_file(swift, "testdata.txt", 0, NULL, NULL);
 	if (scerr != SCERR_SUCCESS) {
 		return scerr;
 	}
 
-	scerr = swift_set_object(context, SWIFT_OBJECT);
+	scerr = swift_delete_object(swift);
 	if (scerr != SCERR_SUCCESS) {
 		return scerr;
 	}
 
-	scerr = swift_put_file(context, "testdata.txt", 0, NULL, NULL);
-	if (scerr != SCERR_SUCCESS) {
-		return scerr;
-	}
-
-	scerr = swift_delete_object(context);
-	if (scerr != SCERR_SUCCESS) {
-		return scerr;
-	}
-
-	scerr = swift_delete_container(context);
+	scerr = swift_delete_container(swift);
 	if (scerr != SCERR_SUCCESS) {
 		return scerr;
 	}
@@ -71,7 +62,10 @@ int
 main(int argc, char **argv)
 {
 	swift_context_t swift;
+	keystone_context_t keystone;
 	enum swift_error scerr;
+	enum keystone_error kserr;
+	const char *swift_url;
 
 	if (argc != 5) {
 		fprintf(stderr, USAGE, argv[0]);
@@ -79,34 +73,92 @@ main(int argc, char **argv)
 	}
 
 	memset(&swift, 0, sizeof(swift));
+	memset(&keystone, 0, sizeof(keystone));
 
 	scerr = swift_global_init();
 	if (scerr != SCERR_SUCCESS) {
 		return EXIT_FAILURE;
 	}
+	atexit(swift_global_cleanup);
+
+	kserr = keystone_global_init();
+	if (kserr != KSERR_SUCCESS) {
+		return EXIT_FAILURE;
+	}
+	atexit(keystone_global_cleanup);
 
 	scerr = swift_start(&swift);
 	if (scerr != SCERR_SUCCESS) {
-		swift_global_cleanup();
+		return EXIT_FAILURE;
+	}
+
+	kserr = keystone_start(&keystone);
+	if (kserr != KSERR_SUCCESS) {
+		swift_end(&swift);
 		return EXIT_FAILURE;
 	}
 
 	scerr = swift_set_debug(&swift, 1);
 	if (scerr != SCERR_SUCCESS) {
 		swift_end(&swift);
-		swift_global_cleanup();
-		return scerr;
+		keystone_end(&keystone);
+		return EXIT_FAILURE;
 	}
 
-	scerr = test_swift_client(&swift, argv[1], argv[2], argv[3], argv[4]);
+	kserr = keystone_set_debug(&keystone, 1);
+	if (kserr != KSERR_SUCCESS) {
+		swift_end(&swift);
+		keystone_end(&keystone);
+		return EXIT_FAILURE;
+	}
+
+	scerr = swift_set_proxy(&swift, PROXY);
 	if (scerr != SCERR_SUCCESS) {
 		swift_end(&swift);
-		swift_global_cleanup();
+		keystone_end(&keystone);
+		return EXIT_FAILURE;
+	}
+
+	kserr = keystone_set_proxy(&keystone, PROXY);
+	if (kserr != KSERR_SUCCESS) {
+		swift_end(&swift);
+		keystone_end(&keystone);
+		return EXIT_FAILURE;
+	}
+
+	kserr = keystone_authenticate(&keystone, argv[1], argv[2], argv[3], argv[4]);
+	if (kserr != KSERR_SUCCESS) {
+		swift_end(&swift);
+		keystone_end(&keystone);
+		return EXIT_FAILURE;
+	}
+
+	scerr = swift_set_auth_token(&swift, keystone_get_auth_token(&keystone));
+	if (scerr != SCERR_SUCCESS) {
+		swift_end(&swift);
+		keystone_end(&keystone);
+		return EXIT_FAILURE;
+	}
+
+	swift_url = keystone_get_service_url(&keystone, OS_SERVICE_SWIFT, 0);
+	fprintf(stderr, "Swift is at: %s\n", swift_url);
+
+	scerr = swift_set_url(&swift, swift_url);
+	if (scerr != SCERR_SUCCESS) {
+		swift_end(&swift);
+		keystone_end(&keystone);
+		return EXIT_FAILURE;
+	}
+
+	scerr = test_swift_client(&swift);
+	if (scerr != SCERR_SUCCESS) {
+		swift_end(&swift);
+		keystone_end(&keystone);
 		return EXIT_FAILURE;
 	}
 
 	swift_end(&swift);
-	swift_global_cleanup();
+	keystone_end(&keystone);
 
 	return EXIT_SUCCESS;
 }
