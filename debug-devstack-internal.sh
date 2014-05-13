3788678a382640c27d22885e4021932c5ec38216
Matt <ukyg9e5r6k7gubiekd6@yahoo.com>
2014-05-13 13:49:03 -0400

    Add scripts to run and debug inside a DevStack VM.

---
+++ debug-devstack-internal.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+scriptdir=$( dirname "$0" )
+. "$scriptdir/devstack-internalrc"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$scriptdir/../swift-client/Debug:$scriptdir/../keystone-client/Debug
+gdb --args "$scriptdir/Debug/test-swift-client" --verbose
